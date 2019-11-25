8e62bc08bcf1869cdff137f89f0de2b59b7e79b7
Walkplay <ivshar@ukr.net>
2019-11-25 08:12:28 +0200

    [MOD] Serialize Point struct

--- KNN/Assets/Source/Data/Point.cs
+++ KNN/Assets/Source/Data/Point.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Data
 {
+    [Serializable]
     public struct Point
     {
         public Vector3 Position { get; private set; }
