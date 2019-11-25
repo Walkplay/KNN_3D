593f115e40ae6cd032e807ae4f017bc0c5ee5e42
Walkplay <ivshar@ukr.net>
2019-11-25 08:13:20 +0200

    [ADD] init KNN config

---
+++ KNN/Assets/Source/Data/KNNConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Data
+{
+    public class KNNConfig : ScriptableObject
+    {
+        public int K = 0;
+        public List<Point> unClassified = new List<Point>();
+        public List<Point> classified = new List<Point>();
+    }
+}
